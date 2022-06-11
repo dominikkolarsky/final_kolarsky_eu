7ec4176b9c3651837b315042c19a2605ab3b76af
dominikkolarsky <kolarskydominik@gmail.com>
2022-06-11 19:42:23 +0200

    skills DONE

---
+++ main.js
@@ -0,0 +1,21 @@
+const toggle = document.getElementById('menu-btn');
+const navbar = document.getElementById('menu')
+
+
+document.onclick = function (e) {
+	if (e.target.className =='menu-link p-2 px-32 my-2 text-secondary') {
+		toggle.classList.remove('open')
+		navbar.classList.remove('flex')
+		navbar.classList.add('hidden')
+	}
+}
+
+
+// toogle hamburger
+toggle.onclick = function () {
+	toggle.classList.toggle('open')
+	navbar.classList.toggle('flex')
+	navbar.classList.toggle('hidden')
+
+
+}
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        'body': '',
+        'mobile-menu': '#141414',
+        'theme': '#1d4ed8',
+        'secondary': '#999999',
+
+        'input-border': '#565666',
+        'input': '#2A2A35',
+        'apps': '#ffffff1c'
+      },
+      fontFamily: {
+        'poppins': ["'Poppins'", 'sans-serif']
+      }
+    },
+  },
+
+  plugins: [],
+}
